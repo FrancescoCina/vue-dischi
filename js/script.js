e4bc85b411a7c4e14e5d74c2d449223a6449772a
FrancescoCina <francesco.cina04@gmail.com>
2021-09-02 18:43:36 +0200

    Fatto ordinamento per data e filtro sulla select

--- js/script.js
+++ js/script.js
@@ -6,15 +6,50 @@ const app = new Vue({
   el: '#app',
   data: {
     albums: [],
+    genSelect: "all",
+    filteredAlbums: [],
   },
-  methods: {},
+  computed: {
+    albumsSortByYear() {
+      return this.albums.sort(function (a, b) {
+        return a.year - b.year;
+      });
+    },
+    gensArray() {
+      let duplicatedArray = [];
+      this.albums.forEach(album => {
+        duplicatedArray.push(album.genre);
+      });
+      const allString = "all";
+      let singleArray = [];
+      duplicatedArray.forEach(element => {
+        if (!singleArray.includes(element)) {
+          singleArray.push(element);
+        }
+      });
+      singleArray.unshift(allString);
+      return singleArray;
+    },
+  },
+  methods: {
+    filterByGenres() {
+      if (this.genSelect === "all") {
+        return this.filteredAlbums = this.albumsSortByYear;
+
+      }
+      this.filteredAlbums = this.albumsSortByYear.filter((album) => {
+        return album.genre === this.genSelect;
+      });
+      console.log(this.filteredAlbums);
+      return this.filteredAlbums;
+    },
+
+  },
   created() {
     axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
       let risp = res.data.response;
-      for (let i = 0; i < 10; i++) {
-        this.albums.push(risp[i]);
-      }
-      console.log(this.albums);
+      this.albums = risp;
+
     })
   }
 });
