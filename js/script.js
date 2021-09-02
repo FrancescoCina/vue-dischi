b7db6f82a028ee8453cf3296cb078026cc9cffa1
FrancescoCina <francesco.cina04@gmail.com>
2021-09-02 16:20:37 +0200

    Stampate tutti gli album in HTML

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    albums: [],
+  },
   methods: {},
+  created() {
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+      let risp = res.data.response;
+      for (let i = 0; i < 10; i++) {
+        this.albums.push(risp[i]);
+      }
+      console.log(this.albums);
+    })
+  }
 });
